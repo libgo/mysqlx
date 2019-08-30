6e40abab94af244af86985be8e82c0364a7cdfdb
elvizlai <sdrzlyz@gmail.com>
2019-08-30 13:53:27 +0800

    fix: log optimize

--- hook.go
+++ hook.go
@@ -22,33 +22,24 @@ func init() {
 type Hook struct {
 }
 
+type sqlTimer struct{}
+
 // Before hook will print the query with it's args and return the context with the timestamp
 func (h *Hook) Before(ctx context.Context, _ string, _ ...interface{}) (context.Context, error) {
-	return context.WithValue(ctx, "x-sql-begin", time.Now()), nil
+	return context.WithValue(ctx, sqlTimer{}, time.Now()), nil
 }
 
 // After hook will get the timestamp registered on the Before hook and print the elapsed time
 func (h *Hook) After(ctx context.Context, query string, args ...interface{}) (context.Context, error) {
-	startAt, ok := ctx.Value("x-sql-begin").(time.Time)
+	startAt, ok := ctx.Value(sqlTimer{}).(time.Time)
 	if !ok {
 		return ctx, nil
 	}
 
-	logger := logx.KVPair(map[string]interface{}{
-		"span": "sql",
-		"took": nanoToMs(time.Since(startAt).Nanoseconds()),
-	})
-
-	if tid, ok := ctx.Value("x-request-id").(string); ok {
-		logger = logger.Trace(tid)
-	}
+	logger := logx.FromContext(ctx)
 
 	if logger.DebugEnabled() {
-		logger.Debugf("> %s. %v", query, args)
-		if soarEnable {
-			out, _ := soar(query)
-			logger.Debugf("%s", out)
-		}
+		logger.KV("span", "sql", "took", nanoToMs(time.Since(startAt).Nanoseconds())).Debugf("> %s. %v", query, args)
 	}
 
 	return ctx, nil
